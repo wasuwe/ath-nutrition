c0d90446d40aac0b1685bd118edf3cfb230fbcbe
wasuwe <40377567+wasuwe@users.noreply.github.com>
2018-08-16 16:13:33 +0700

    Food_Order

---
+++ index.php
@@ -0,0 +1,185 @@
+
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Title Page</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        
+    </head>
+    <body >
+    
+    <!-- <div class=""> -->
+        <div class="col-md-6 col-md-offset-3 ">
+        <form action="" >
+        
+        <div class="form-row ">
+            <div class="form-group col-md-3 ">
+                <label>Hn </label>
+                <input type="text" class="form-control" name="" placeholder=""> 
+            </div>
+            <div class="form-group col-md-3">
+                <label>An </label>
+                <input type="text" class="form-control" name="" placeholder="">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="">ชื่อ - นามสกุล </label>
+                <input type="text" class="form-control" name="" placeholder="">
+            </div>
+        </div>
+
+        <div class="form-row">
+            <div class="form-group col-md-6 ">
+                <label>อายุ </label>
+                <input type="text" class="form-control" name="" placeholder="">
+            </div>
+            <div class="form-group  col-md-6">
+            <label for="">สิทธิ</label>
+                <input type="text" class="form-control" name="" placeholder="">
+            </div>
+        </div>
+
+        <div class="form-row ">
+            <div class="form-group col-md-5">
+                <label>ตึก</label>
+                <input type="text" class="form-control" name="" placeholder="">
+            </div>
+            <div class="form-group col-md-3">
+            <label for="">เลขห้อง/เลขเตียง</label>
+                <input type="text" class="form-control" name="" placeholder="">
+            </div>
+            <div class="form-group col-md-4">
+            <label for="">วันที่</label>
+                <input type="date" class="form-control" name="" placeholder="">
+            </div>
+        </div>
+        
+       
+        
+                       
+            <div class="form-row">
+
+                <div class="form-row">
+                    <div class="form-group col-md-2">
+                    <label>มื้อเช้า</label> 
+                    </div>
+                    
+                    <div class="form-group col-md-4">
+                    <label>มื้ออื่นที่เหมือนกับมื้อเช้า </label>
+                    </div>
+
+                    <div class="form-group col-md-3 ">                
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">มื้อกลางวัน</label>
+                    </div>
+
+                    <div class="form-group col-md-3 ">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">มื้อกลางวัน</label>
+                    </div>
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label>ประเภทอาหาร : </label>
+                    <select class="form-control" name="">
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>ประเภทอาหารย่อย</label>
+                    <select class="form-control" name="" >
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-12">
+                    <label>ข้อความ</label>
+                    <textarea class="form-control" name="" rows="3"></textarea>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                <label>มื้อกลางวัน</label> 
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>ประเภทอาหาร : </label>
+                    <select class="form-control" name="">
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>ประเภทอาหารย่อย</label>
+                    <select class="form-control" name="">
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-12">
+                    <label>ข้อความ</label>
+                    <textarea class="form-control" name="" rows="3"></textarea>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                <label>มื้อเย็น</label> 
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>ประเภทอาหาร : </label>
+                    <select class="form-control" name="">
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>ประเภทอาหารย่อย</label>
+                    <select class="form-control" name="">
+                        <option>Default select</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-12">
+                    <label>ข้อความ</label>
+                    <textarea class="form-control" name="" rows="3"></textarea>
+                </div>
+            </div>
+
+            <center> 
+                <button type="submit" align="center" class="btn btn-primary" name="">บันทึก</button>
+            </center>
+
+                
+
+            
+        </form>
+        </div>
+    </div>
+    
+        
+
+
+
+
+
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
